a878ecee9aacf867cc9fbd5b8323ec07f8fca22f
zerodev6 <zerodev699@gmail.com>
2026-02-10 16:08:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ class Config(object):
  
     # other configs
     BOT_UPTIME  = time.time()
-    START_PIC   = ""
+    START_PIC   = "https://i.ibb.co/S4Y8w1NT/img-8108646188.jpg"
     ADMIN="8498741978 @venuboyy"
 
     # channels logs
