5fc7c0d4dcab3365ff52531594556c6115906da8
zerodev6 <zerodev699@gmail.com>
2026-04-03 16:28:54 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,3 @@
-import os
 from datetime import datetime
 from pytz import timezone
 from pyrogram import Client, __version__
@@ -32,11 +31,10 @@ class Bot(Client):
         self.username = me.username  
         self.uptime = Config.BOT_UPTIME     
         if Config.WEBHOOK:
-            port = int(os.environ.get("PORT", 8080))
             app = web.AppRunner(await web_server())
             await app.setup()       
-            await web.TCPSite(app, "0.0.0.0", port).start()
-            print(f"Web server started on port {port}")
+            await web.TCPSite(app, "0.0.0.0", 10000).start()
+            print("Web server started on port 10000")
         print(f"{me.first_name} Is Started.....✨️")
         for id in Config.ADMIN:
             try: await self.send_message(id, f"**{me.first_name}  Is Started...**")                                
